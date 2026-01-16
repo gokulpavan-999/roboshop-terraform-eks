396974fcbb1c5988a50d07dbd4f1b1e7a5f2c854
gokulpavan-999 <pavanthota480@gmail.com>
2026-01-16 12:44:02 +0530

    Create variables.tf

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "pavandevops.fun"
+}
+
+variable "zone_id" {
+    default = "Z08654702BW8GGH0J30B"
+}
