16b04453a0ad210be2199b7d11bfef2be0776c44
gokulpavan-999 <pavanthota480@gmail.com>
2026-01-16 12:53:59 +0530

    Create variables.tf

---
+++ 90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08654702BW8GGH0J30B"
+}
+
+variable "domain_name" {
+    default = "pavandevops.fun"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
